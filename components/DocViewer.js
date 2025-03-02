71b48084d47aab453472647565be5459877476ce
Aryan26-bit <aryaniit26@gmail.com>
2025-03-02 23:59:40 +0530

    added_changes_pdf_replaced_docx

--- components/DocViewer.js
+++ components/DocViewer.js
@@ -1,17 +1,35 @@
-import { useState, useCallback } from "react";
+import React from 'react';
+import { useState, useCallback, useRef, useEffect } from "react";
+import { renderAsync } from "docx-preview";
 
 const documents = [
-  { url: "/docs/doc_1.pdf", name: "FIRST DOC" },
-  { url: "/docs/doc_2.pdf", name: "SECOND DOC" },
-  { url: "/docs/doc_3.pdf", name: "THIRD DOC" },
+  { url: "/docs/doc_1.docx", name: "FIRST DOC" },
+  { url: "/docs/doc_2.docx", name: "SECOND DOC" },
+  { url: "/docs/doc_3.docx", name: "THIRD DOC" },
 ];
 
 const DocViewer = () => {
   const [selectedDoc, setSelectedDoc] = useState(null);
+  const docViewerRef = useRef(null);
 
-  const handleSelectDoc = useCallback((url) => {
-    setSelectedDoc(url);
+  const handleSelectDoc = useCallback(async (url) => {
+    try {
+      const response = await fetch(url);
+      const arrayBuffer = await response.arrayBuffer();
+      setSelectedDoc(arrayBuffer);
+    } catch (error) {
+      console.error("Error loading doc", error);
+      alert("No preview available");
+    }
   }, []);
+
+  useEffect(() => {
+    if (selectedDoc && docViewerRef.current) {
+      renderAsync(selectedDoc, docViewerRef.current)
+        .then(() => console.log("Document rendered."))
+        .catch((error) => console.error("Error rendering in doc", error));
+    }
+  }, [selectedDoc]);
 
   return (
     <div className="view-document">
@@ -35,14 +53,16 @@ const DocViewer = () => {
             Reset Viewer
           </button>
 
-          <iframe
-            src={selectedDoc}
-            width="100%"
-            height="600px"
-            className="selected-doc"
-            title="Document Viewer"
-            loading="lazy"
-            onError={({ target }) => (target.style.display = "none")}
+          <div
+            ref={docViewerRef}
+            style={{
+              width: "100%",
+              height: "600px",
+              border: "1px solid #ccc",
+              overflowY: "auto",
+              padding: "10px",
+              backgroundColor: "#fff",
+            }}
           />
         </>
       )}
